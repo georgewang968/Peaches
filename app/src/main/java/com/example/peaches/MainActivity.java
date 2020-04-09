ddd30a125483dc62083707f604b45ee9a89c98e2
georgewang968 <61836156+georgewang968@users.noreply.github.com>
2020-04-08 23:55:49 -0400

    notification button app

--- app/src/main/java/com/example/peaches/MainActivity.java
+++ app/src/main/java/com/example/peaches/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.peaches;
 //test message
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+
+import static androidx.core.app.NotificationCompat.PRIORITY_HIGH;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CHANNEL_ID = "peaches_1";
+    public static final String CHANNEL_NAME = "Peaches";
+    public static final String CHANNEL_DESC = "Peaches Notification Channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+        }
+
+        findViewById(R.id.buttonNotify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+
+    }
+
+    private void displayNotification(){
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_peach)
+                    .setContentTitle("Peaches")
+                    .setContentText("Ben has a chode dick.")
+                    .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(this);
+        mNotificationMgr.notify(1, mBuilder.build());
+
     }
 }
