2207e4f1aaf2d1d147c0a431b84ce818451e4d7d
Exscidium1 <zachary.wiencko@gmail.com>
2020-04-10 18:55:23 -0400

    Added temporary randomness to reading lines. The uniqueness still needs to be added but now the program can give notifications for all lines.

--- app/src/main/java/com/example/peaches/MessageManager.java
+++ app/src/main/java/com/example/peaches/MessageManager.java
@@ -7,21 +7,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.Math;
 
 public class MessageManager {
 
     private final static String fileName = "messages.txt";
     private final static String TAG = MessageManager.class.getName();
 
+
     public static String ReadFile(Context context){
         //final String path = context.getFilesDir().getAbsolutePath();
         String line = "";
 
         try {
+            // open file
             BufferedReader reader = null;
             reader = new BufferedReader(new InputStreamReader(context.getAssets().open(fileName)));
 
+            // get random line
+            // first find length of file
+            int totalLines = 0;
+            while (reader.readLine() != null)
+                totalLines++;
+            reader.close();
+
+            // now pull random line
+            reader = new BufferedReader(new InputStreamReader(context.getAssets().open(fileName)));
+            int pos = (int)(Math.random() * totalLines);
+            for (int i = 0; i < pos; i++)
+                reader.readLine();
+
             line = reader.readLine();
+
             reader.close();
         }
         catch(FileNotFoundException e){
